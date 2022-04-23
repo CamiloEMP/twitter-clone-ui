6a98625dbb867faf30605d144a5cdd27a8b87861
b7c8p <morapaezc@gmail.com>
2022-04-22 21:13:30 -0500

    fix styles and add new tweet

--- src/components/Tweets/CardTweet/index.jsx
+++ src/components/Tweets/CardTweet/index.jsx
@@ -1,38 +1,55 @@
 import style from './index.module.css'
 import * as Icon from './icons'
 
-export const CardTweet = ({ img, author, username, hour, content, likes, retweet, comments }) => {
+export const CardTweet = ({
+  img,
+  author,
+  username,
+  hour,
+  content,
+  likes,
+  retweet,
+  comments,
+  contentImage,
+}) => {
   return (
     <a className={style.CardTweet} href="#">
-      <div>
-        <img alt={`${username} tweet`} src={img} />
-      </div>
-      <div className={style.tweetContent}>
+      <section>
         <div>
-          <span>{author}</span>
-          <span>{username}</span>
-          <span>·</span>
-          <span>{hour}</span>
+          <img alt={`${username} tweet`} src={img} />
         </div>
-        <p className={style.contentTweet}>{content}</p>
-        <div className={style.icons}>
-          <div className={style.active}>
-            <Icon.Reply />
-            <span>{likes}</span>
+        <div className={style.tweetContent}>
+          <div>
+            <span>{author}</span>
+            <span>{username}</span>
+            <span>·</span>
+            <span>{hour}</span>
           </div>
-          <div className={style.active}>
-            <Icon.Retweet />
-            <span>{retweet}</span>
-          </div>
-          <div className={style.active}>
-            <Icon.Like />
-            <span>{comments}</span>
-          </div>
-          <div className={style.active}>
-            <Icon.Share />
+          <p className={style.contentTweet}>{content}</p>
+          {contentImage && (
+            <div className={style.contentImage}>
+              <img alt={`${username} tweet`} src={contentImage} />
+            </div>
+          )}
+          <div className={style.icons}>
+            <div className={style.active}>
+              <Icon.Reply />
+              <span>{likes}</span>
+            </div>
+            <div className={style.active}>
+              <Icon.Retweet />
+              <span>{retweet}</span>
+            </div>
+            <div className={style.active}>
+              <Icon.Like />
+              <span>{comments}</span>
+            </div>
+            <div className={style.active}>
+              <Icon.Share />
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </a>
   )
 }
--- src/components/Tweets/index.jsx
+++ src/components/Tweets/index.jsx
@@ -1,12 +1,17 @@
 import tweets from '../../data/index.json' // Simulate deta of twitter
 
+import style from './index.module.css'
 import { CardTweet } from './CardTweet'
 
 export const Tweets = () => {
   return (
     <section>
       {tweets.map(tweet => (
-        <article key={tweet.username}>
+        <article
+          key={tweet.username}
+          className={style.Tweet}
+          style={{ borderBottom: '1px solid #2f3336' }}
+        >
           <CardTweet {...tweet} />
         </article>
       ))}
